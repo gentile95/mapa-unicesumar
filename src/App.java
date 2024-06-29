600ccf33380cb5521a09a0c448723bb3399832cc
gentile95 <lucas.gentile95@gmail.com>
2024-06-29 10:31:39 -0300

    Atualizando código em nova branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Pessoa pessoa = new Pessoa("Lucas", 28, (float) 1.84);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+        System.out.println(pessoa.getAltura());
+
+        pessoa.setNome("Thais");
+        pessoa.setIdade(26);
+        pessoa.setAltura((float) 1.70);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+        System.out.println(pessoa.getAltura());
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,32 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private float altura;
+
+    public Pessoa(String nome, int idade, float altura){
+        this.nome = nome;
+        this.altura = altura;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public float getAltura() {
+        return altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+}
+
+
